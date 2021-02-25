84d5038bd23e0e1c1955648cbc3b2ceba270b613
rvp <rodrigovaz.pires@qomplx.com>
2021-02-25 23:13:30 +0000

    Google Maps working and initial plotting working

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,17 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#04005B',
+        secondary: '#b0bec5',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+      },
+    },
+  },
+})
+
+export default vuetify
